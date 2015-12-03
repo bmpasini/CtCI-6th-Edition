9db35724c6b345a05880c5fb6ef93f653d40170f
bmpasini <bmpasini@gmail.com>
2015-12-03 12:13:05 -0500

    add alternative solution to question 8.13

--- chap-8/8.13.py
+++ chap-8/8.13.py
@@ -7,27 +7,45 @@ from random import random
 from math import floor
 from pprint import pprint
 
-def stack_of_boxes(boxes):
+def get_max_height(boxes):
     boxes = sorted(boxes, key=lambda x: x['height'], reverse=True)
     max_height = 0
     max_heights = [ None for _ in range(len(boxes)) ]
     for i in range(len(boxes)):
-        height = _stack_of_boxes(boxes, i, max_heights)
+        height = _get_max_height(boxes, i, max_heights)
         max_height = max(height, max_height)
     return max_height
 
-def _stack_of_boxes(boxes, i, max_heights):
+def _get_max_height(boxes, i, max_heights):
     if max_heights[i] is not None:
         return max_heights[i]
     bottom = boxes[i]
     max_height = 0
     for j in range(i+1, len(boxes)):
         if meet_requirements(bottom, boxes[j]):
-            height = _stack_of_boxes(boxes, j, max_heights)
+            height = _get_max_height(boxes, j, max_heights)
             max_height = max(height, max_height)
     max_height += bottom['height']
     max_heights[i] = max_height
     return max_height
+
+def get_max_height2(boxes):
+    boxes = sorted(boxes, key=lambda x: x['height'], reverse=True)
+    max_heights = [ 0 for _ in range(len(boxes)) ]
+    return _get_max_height2(boxes, None, 0, max_heights)
+
+def _get_max_height2(boxes, bottom, offset, max_heights):
+    if offset >= len(boxes):
+        return 0
+    new_bottom = boxes[offset]
+    height_with_bottom = 0
+    if bottom is None or meet_requirements(bottom, new_bottom):
+        if max_heights[offset] == 0:
+            max_heights[offset] = _get_max_height2(boxes, new_bottom, offset+1, max_heights)
+            max_heights[offset] += new_bottom['height']
+        height_with_bottom = max_heights[offset]
+    height_without_bottom = _get_max_height2(boxes, bottom, offset+1, max_heights)
+    return max(height_with_bottom, height_without_bottom)
 
 def meet_requirements(bottom, top):
     return bottom['height'] > top['height'] and \
@@ -43,4 +61,7 @@ if __name__ == "__main__":
                          1+floor(200*random())
                         ) for _ in range(0,10) ]
     pprint(boxes)
-    pprint(stack_of_boxes(boxes))    
+    pprint(get_max_height(boxes))
+    pprint(get_max_height2(boxes))
+
+
